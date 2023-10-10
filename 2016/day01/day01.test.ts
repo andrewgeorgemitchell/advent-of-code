5e7d69e3e678eace8a8c419e7dfb0c4dbeebb9d5
Andrew <andrewgeorgemitchell@gmail.com>
2023-10-10 08:59:37 -0700

    Make tests more readable

--- 2016/day01/day01.test.ts
+++ 2016/day01/day01.test.ts
@@ -19,22 +19,22 @@ const testInput3 = [
   ['R', 3],
 ]
 
-test('Part 1: Test input1 answer', async () => {
+test('Day 01 - Part 1: Test input1 answer', async () => {
   const result = day01.findQuickestRoute(testInput1 as any)
   expect(result).toBe(5)
 })
 
-test('Part 1: Test input2 answer', async () => {
+test('Day 01 - Part 1: Test input2 answer', async () => {
   const result = day01.findQuickestRoute(testInput2 as any)
   expect(result).toBe(2)
 })
 
-test('Part 1: Test input3 answer', async () => {
+test('Day 01 - Part 1: Test input3 answer', async () => {
   const result = day01.findQuickestRoute(testInput3 as any)
   expect(result).toBe(12)
 })
 
-test('Part 1: Real input answer', async () => {
+test('Day 01 - Part 1: Real input answer', async () => {
   const input = await day01.parseFileInput()
   const result = day01.findQuickestRoute(input)
   expect(result).toBe(161)
@@ -47,12 +47,12 @@ const p2TestInput = [
   ['R', 8],
 ]
 
-test('Part 2: Test input answer', async () => {
+test('Day 01 - Part 2: Test input answer', async () => {
   const result = day01.findTwiceVisitedPoint(p2TestInput as any)
   expect(result).toBe(4)
 })
 
-test('Part 2: Real input answer', async () => {
+test('Day 01 - Part 2: Real input answer', async () => {
   const input = await day01.parseFileInput()
   const result = day01.findTwiceVisitedPoint(input)
   expect(result).toBe(110)
